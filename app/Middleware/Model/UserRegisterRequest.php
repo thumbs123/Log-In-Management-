36f0fdd319d027231bf867b0e3081d517d29bfb2
kekeputri08 <113098935+kekeputri08@users.noreply.github.com>
2023-11-05 22:42:56 +0700

    Create UserRegisterRequest.php
    vidio 13-22

---
+++ app/Middleware/Model/UserRegisterRequest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ProgrammerZamanNow\Belajar\PHP\MVC\Middleware\Model;
+
+class UserRegisterRequest
+{
+    public ?string $id = null;
+    public ?string $name = null;
+    public ?string $password = null;
+}
