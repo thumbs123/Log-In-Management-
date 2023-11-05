314b80974fe860959b3634d0112a22c14d7bfdc2
kekeputri08 <113098935+kekeputri08@users.noreply.github.com>
2023-11-05 22:42:02 +0700

    Create UserLoginResponse.php
    vidio 13-22

---
+++ app/Middleware/Model/UserLoginResponse.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ProgrammerZamanNow\Belajar\PHP\MVC\Middleware\Model;
+
+use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\User;
+
+class UserLoginResponse
+{
+    public User $user;
+}
